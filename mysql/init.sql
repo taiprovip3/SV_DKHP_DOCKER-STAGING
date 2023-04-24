75abbdce9ea8ece459185f531782c2effe0314af
taiprovip3 <taito1doraemon@gmail.com>
2023-04-24 22:42:08 +0700

    update docker compose

--- mysql/init.sql
+++ mysql/init.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 24, 2023 at 05:25 PM
+-- Generation Time: Apr 24, 2023 at 05:41 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
